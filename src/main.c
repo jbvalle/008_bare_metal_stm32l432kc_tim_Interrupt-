b93b2657bce8736650422e44f3c40563880ed0fc
John Bryan Valle <jbvalle.jv@gmail.com>
2022-10-19 17:59:01 +0200

    implemented tim2

--- src/main.c
+++ src/main.c
@@ -25,34 +25,63 @@ int main(void){
     /*********************/
     /** Configure GPIOB **/
     /*********************/
-
-    /** Enable GPIOB **/
-    RCC->RCC_AHB2ENR |= (1 << 1);
+    /** Enable GPIOA **/
+    RCC->RCC_AHB2ENR |= (1 << 0);
 
     /** Set PB3 as OUTPUT **/
-    GPIOB->GPIOx_MODER &= ~(3 << (3 * 2));
-    GPIOB->GPIOx_MODER |=  (1 << (3 * 2));
-    GPIOB->GPIOx_ODR |= (1 << 3);
+    GPIOB->GPIOx_MODER &= ~(3 << (4 * 2));
+    GPIOB->GPIOx_MODER |=  (1 << (4 * 2));
+    GPIOB->GPIOx_ODR |= (1 << 4);
+
+    /************************/
+    /** TIM2 Configuration **/
+    /************************/
+    RCC->RCC_APB1ENR1 |= (1 << 0);
     
+    /** Set PRE and ARR **/
+    uint32_t toggle_frequency_TIM2 = 8;
+
+    TIM2->TIMx_PSC = 16000 - 1;
+
+    TIM2->TIMx_ARR = (HSI_FREQ / (toggle_frequency_TIM2 * TIM2->TIMx_PSC)) - 1;
+
+    /** Set Compare Value **/
+    TIM2->TIMx_CCR1 = 1;
+
+    /** Enable Compare Register **/
+    TIM2->TIMx_CCER |= 1;
+
+    /** Reset TIM6 CNT **/
+    TIM2->TIMx_CNT = 0;
+
+    /** Start Counter **/
+    TIM2->TIMx_CR1 |= 1;
+
+    /** Enable Interrupt **/
+    TIM2->TIMx_DIER |= (1 << 1);
+
+    /** Enable IRQ in NVIC **/
+    NVIC->ISER[0] |= (1 << (TIM2_IRQn & 32));
+
     /************************/
     /** TIM6 Configuration **/
     /************************/
 
     /** Enable TIM6 **/
-    RCC->RCC_APB1ENR1 |= (1 << 1);
+    //RCC->RCC_APB1ENR1 |= (1 << 1);
 
     /** Set PRE and ARR **/
-    uint32_t toggle_frequency_TIM6 = 2;
+    //uint32_t toggle_frequency_TIM6 = 2;
 
-    TIM6->TIMx_PSC = 16000 - 1;
+    //TIM6->TIMx_PSC = 16000 - 1;
 
-    TIM6->TIMx_ARR = (HSI_FREQ / (toggle_frequency_TIM6 * TIM6->TIMx_PSC)) - 1;
+    //TIM6->TIMx_ARR = (HSI_FREQ / (toggle_frequency_TIM6 * TIM6->TIMx_PSC)) - 1;
 
     /** Reset TIM6 CNT **/
-    TIM6->TIMx_CNT = 0;
+    //TIM6->TIMx_CNT = 0;
 
     /** Start Counter **/
-    TIM6->TIMx_CR1 |= 1;
+    //TIM6->TIMx_CR1 |= 1;
 
     for(;;){
 
